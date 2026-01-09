e502338cd385b7f4ce7b54d1732b007ad1d1b670
Marcos Bolivar <marcosbolivar2007@gmail.com>
2026-01-09 16:14:42 +0100

    Añadido boton de salir, arreglado el formato.

--- entregas/bolivarMarcos/src/ConversorUnidades.java
+++ entregas/bolivarMarcos/src/ConversorUnidades.java
@@ -5,23 +5,26 @@ public class ConversorUnidades {
         Scanner sc = new Scanner(System.in);
         boolean operacionValida = true;
         int eleccionConversion = 0;
-        double valorConversion= 0.0;
+        double valorConversion = 0.0;
 
-       while (operacionValida){
+        while (operacionValida) {
 
-        eleccionConversion= pedirConversion(sc, eleccionConversion);
+            eleccionConversion = pedirConversion(sc, eleccionConversion);
 
-        if (eleccionConversion == 1) {
-            convertirLongitud(sc, eleccionConversion, valorConversion, operacionValida);
-        } else if (eleccionConversion == 2) {
-            convertirPeso(sc, eleccionConversion, valorConversion, operacionValida);
-        } else if (eleccionConversion == 3) {
-            convertirTemperatura(sc, eleccionConversion, valorConversion, operacionValida);
-        } else {
-            System.out.println("Tipo no valido");
+            if (eleccionConversion == 1) {
+                convertirLongitud(sc, eleccionConversion, valorConversion, operacionValida);
+            } else if (eleccionConversion == 2) {
+                convertirPeso(sc, eleccionConversion, valorConversion, operacionValida);
+            } else if (eleccionConversion == 3) {
+                convertirTemperatura(sc, eleccionConversion, valorConversion, operacionValida);
+            }else if (eleccionConversion==4){
+                System.out.println("Saliendo del conversor de unidades...");
+                operacionValida=false;
+            }else {
+                System.out.println("Tipo no valido");
+            }
         }
-       } 
-        
+
     }
 
     static int pedirConversion(Scanner sc, int eleccionConversion) {
@@ -29,6 +32,7 @@ public class ConversorUnidades {
         System.out.println("[1] Longitud");
         System.out.println("[2] Peso");
         System.out.println("[3] Temperatura");
+        System.out.println("[4] Salir");
         System.out.print("Tipo de conversion: ");
         eleccionConversion = sc.nextInt();
         return eleccionConversion;
@@ -36,7 +40,7 @@ public class ConversorUnidades {
 
     static double convertirLongitud(Scanner sc, int eleccion, double conversion, boolean operacionValida) {
         while (operacionValida) {
-        System.out.println("Conversion de longitud:");
+            System.out.println("Conversion de longitud:");
             System.out.println("[1] Metros a Kilometros");
             System.out.println("[2] Kilometros a Metros");
             System.out.println("[3] Metros a Millas");
@@ -46,10 +50,10 @@ public class ConversorUnidades {
             System.out.println("[7] Volver al menu principal");
             System.out.print("Opcion: ");
             eleccion = sc.nextInt();
-            if (eleccion >=7){
+            if (eleccion >= 7) {
                 System.out.println("Volviendo al menu principal...");
                 operacionValida = false;
-                break; 
+                break;
             }
             System.out.print("Valor: ");
             conversion = sc.nextDouble();
@@ -75,12 +79,9 @@ public class ConversorUnidades {
         return conversion;
     }
 
-
-
-    
     static double convertirPeso(Scanner sc, int eleccion, double conversion, boolean operacionValida) {
         while (operacionValida) {
-        System.out.println("Conversion de peso:");
+            System.out.println("Conversion de peso:");
             System.out.println("[1] Kilogramos a Gramos");
             System.out.println("[2] Gramos a Kilogramos");
             System.out.println("[3] Kilogramos a Libras");
@@ -90,7 +91,7 @@ public class ConversorUnidades {
             System.out.println("[7] Volver al menu principal");
             System.out.print("Opcion: ");
             eleccion = sc.nextInt();
-            if (eleccion>=7){
+            if (eleccion >= 7) {
                 System.out.println("Volviendo al menu principal...");
                 operacionValida = false;
                 break;
@@ -110,7 +111,7 @@ public class ConversorUnidades {
                 System.out.println(conversion + " g = " + (conversion * 0.035274) + " oz");
             } else if (eleccion == 6) {
                 System.out.println(conversion + " oz = " + (conversion * 28.3495) + " g");
-            }else {
+            } else {
                 System.out.println("volviendo al menu principal...");
                 operacionValida = false;
             }
@@ -122,7 +123,7 @@ public class ConversorUnidades {
 
     static double convertirTemperatura(Scanner sc, int eleccion, double conversion, boolean operacionValida) {
         while (operacionValida) {
-        System.out.println("Conversion de temperatura:");
+            System.out.println("Conversion de temperatura:");
             System.out.println("[1] Celsius a Fahrenheit");
             System.out.println("[2] Fahrenheit a Celsius");
             System.out.println("[3] Celsius a Kelvin");
@@ -130,7 +131,7 @@ public class ConversorUnidades {
             System.out.println("[5] Volver al menu principal");
             System.out.print("Opcion: ");
             eleccion = sc.nextInt();
-            if (eleccion>=5){
+            if (eleccion >= 5) {
                 System.out.println("Volviendo al menu principal...");
                 operacionValida = false;
                 break;
@@ -146,7 +147,7 @@ public class ConversorUnidades {
                 System.out.println(conversion + " C = " + (conversion + 273.15) + " K");
             } else if (eleccion == 4) {
                 System.out.println(conversion + " K = " + (conversion - 273.15) + " C");
-            }else {
+            } else {
                 System.out.println("volviendo al menu principal...");
                 operacionValida = false;
             }
@@ -155,11 +156,4 @@ public class ConversorUnidades {
         return conversion;
     }
 
-    
-
-
-        
-
-    
 }
-
